cc4e22cb011239a7b403e00d2bc5225685295e4c
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-05 14:21:52 -0300

    Feat: Entidade de Departamento

---
+++ src/departments/entities/department.entity.ts
@@ -0,0 +1,22 @@
+import { Course } from "src/courses/entities/course.entity";
+import { User } from "src/users/entities/user.entity";
+import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('departments')
+export class Department {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 255, unique: true, nullable: false })
+    name: string;
+
+    @OneToOne(() => User, {
+        nullable: true,
+        onDelete: 'SET NULL',
+    })
+    @JoinColumn({ name: 'coordinator_id' }) 
+    coordinator: User;
+
+    @OneToMany(() => Course, (course) => course.department)
+    courses: Course[];
+}
