cf94715cc5695e57896477eb95293c610fd1858d
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-05 20:46:20 -0300

    Feat: Adicionando o modulo de Disciplinas

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
 import { CoursesModule } from './courses/courses.module';
 import { DepartmentsModule } from './departments/departments.module';
+import { DisciplinesModule } from './disciplines/disciplines.module';
 
 @Module({
-  imports: [UsersModule, RolesModule, CoursesModule, DepartmentsModule],
+  imports: [UsersModule, RolesModule, CoursesModule, DepartmentsModule, DisciplinesModule],
   controllers: [AppController],
   providers: [AppService],
 })
