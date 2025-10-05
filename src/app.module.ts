cd269e78966afb6175eacdd1dd8478d79f4c53c8
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-05 14:45:29 -0300

    Feat: Adicionando módulos de curso e departamento

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
+import { CoursesModule } from './courses/courses.module';
+import { DepartmentsModule } from './departments/departments.module';
 
 @Module({
-  imports: [UsersModule, RolesModule],
+  imports: [UsersModule, RolesModule, CoursesModule, DepartmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
