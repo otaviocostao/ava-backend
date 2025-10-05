d1ed9cd1c405daa369eb78c90f460dd85a818a09
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-05 12:11:16 -0300

    Feat: Service de Roles com métodos de CRUD

---
+++ src/roles/roles.service.ts
@@ -0,0 +1,69 @@
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from './entities/role.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class RolesService {
+
+  constructor (
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>
+  ) {}
+
+  // Criar nova Role
+  async create(createRoleDto: CreateRoleDto) {
+    const existingRole = await this.roleRepository.findOneBy({
+      name: createRoleDto.name,
+    })
+
+    if(existingRole){
+      throw new ConflictException('Esta Role já foi criada.')
+    }
+
+    const role = this.roleRepository.create(createRoleDto);
+
+    return await this.roleRepository.save(role);
+  }
+
+  // Buscar todas as Roles
+  findAll() : Promise<Role[]> {
+
+    return this.roleRepository.find();
+  }
+
+  // Buscar Role por id
+  async findOne(id: string): Promise<Role> {
+    const role = await this.roleRepository.findOneBy({ id });
+
+    if(!role){
+      throw new NotFoundException(`Role com o ID '${id}' não encontrada.`)
+    }
+    return role;
+  }
+
+  //Atualizar Role
+  async update(id: string, updateRoleDto: UpdateRoleDto): Promise<Role> {
+    const role = await this.roleRepository.preload({ 
+      id,
+      ...updateRoleDto,
+    });
+
+    if(!role){
+      throw new NotFoundException(`Role com o ID '${id}' não encontrada.`)
+    }
+
+    return await this.roleRepository.save(role);
+  }
+
+  // Excluir Role
+  async remove(id: string): Promise<void> {
+    const result = await this.roleRepository.delete(id);
+    
+    if (result.affected === 0) {
+      throw new NotFoundException(`Role com o ID '${id}' não encontrada.`);
+    }
+  }
+}
