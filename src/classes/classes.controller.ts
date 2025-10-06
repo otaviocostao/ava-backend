69fcb2ec21efc62480b9896551ecee12d1933dbf
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-05 21:40:30 -0300

    Feat: Controller de Classes com endpoints

---
+++ src/classes/classes.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, Query, ParseUUIDPipe } from '@nestjs/common';
+import { ClassesService } from './classes.service';
+import { CreateClassDto } from './dto/create-class.dto';
+import { UpdateClassDto } from './dto/update-class.dto';
+
+@Controller('classes')
+export class ClassesController {
+  constructor(private readonly classesService: ClassesService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createClassDto: CreateClassDto) {
+    return this.classesService.create(createClassDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.classesService.findAll();
+  }
+
+ 
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.classesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateClassDto: UpdateClassDto,
+  ) {
+    return this.classesService.update(id, updateClassDto);
+  }
+
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.classesService.remove(id);
+  }
+}
