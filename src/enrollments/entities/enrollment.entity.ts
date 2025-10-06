b9907ac7254b4f38a545eb18d5d524118a053f06
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-05 22:02:39 -0300

    Feat: Entidade de matricula de aluno

---
+++ src/enrollments/entities/enrollment.entity.ts
@@ -0,0 +1,23 @@
+import { Class } from "src/classes/entities/class.entity";
+import { User } from "src/users/entities/user.entity";
+import { CreateDateColumn, Entity, JoinColumn, ManyToMany, ManyToOne, OneToOne, PrimaryGeneratedColumn, Unique } from "typeorm";
+
+@Unique(['student', 'class'])
+@Entity('enrollments')
+export class Enrollment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  //Aluno matriculado
+  @ManyToOne(() => User, { nullable: false, onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'student_id' })
+  student: User;
+
+  // Turma que o aluno está matriculado
+  @ManyToOne(() => Class, { nullable: false, onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'class_id' })
+  class: Class;
+
+  @CreateDateColumn({ name: 'enrolled_at', type: 'timestamp with time zone' })
+  enrolledAt: Date;
+}
