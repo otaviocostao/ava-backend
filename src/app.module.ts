cb3ab6979296ff064b5d8c659e3dce5217040a2a
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-06 22:50:09 -0300

    Feat: Importando módulo de Attendance

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,10 @@ import { DisciplinesModule } from './disciplines/disciplines.module';
 import { ClassesModule } from './classes/classes.module';
 import { EnrollmentsModule } from './enrollments/enrollments.module';
 import { VideoLessonsModule } from './video-lessons/video-lessons.module';
+import { AttendancesModule } from './attendances/attendances.module';
 
 @Module({
-  imports: [UsersModule, RolesModule, CoursesModule, DepartmentsModule, DisciplinesModule, ClassesModule, EnrollmentsModule, VideoLessonsModule],
+  imports: [UsersModule, RolesModule, CoursesModule, DepartmentsModule, DisciplinesModule, ClassesModule, EnrollmentsModule, VideoLessonsModule, AttendancesModule],
   controllers: [AppController],
   providers: [AppService],
 })
