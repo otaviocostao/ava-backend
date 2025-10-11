697f2575cd5597ec074c1a4ff77c8f8b9550c40a
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-11 17:22:39 -0300

    Feat: Enum de dias da semana

---
+++ src/common/enums/day-of-week.enum.ts
@@ -0,0 +1,10 @@
+export enum DayOfWeek {
+    SUNDAY = 'domingo',
+    MONDAY = 'segunda-feira',
+    TUESDAY = 'terca-feira',
+    WEDNESDAY = 'quarta-feira',
+    THURSDAY = 'quinta-feira',
+    FRIDAY = 'sexta-feira',
+    SATURDAY = 'sabado'
+
+}
