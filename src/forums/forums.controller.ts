5f2dc310cd8f62b1c7a33d590152ca3deee26e34
Otavio <dev.otaviocostao@gmail.com>
2025-10-12 17:58:22 -0300

    Feat: Controller com endpoints para a entidade Forum

---
+++ src/forums/forums.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { ForumsService } from './forums.service';
+import { CreateForumDto } from './dto/create-forum.dto';
+import { UpdateForumDto } from './dto/update-forum.dto';
+
+@Controller('forums')
+export class ForumsController {
+  constructor(private readonly forumsService: ForumsService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createForumDto: CreateForumDto) {
+    return this.forumsService.create(createForumDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.forumsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.forumsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateForumDto: UpdateForumDto) {
+    return this.forumsService.update(id, updateForumDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return this.forumsService.remove(id);
+  }
+}
