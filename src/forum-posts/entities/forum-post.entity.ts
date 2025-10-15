b65602f60d114facae74a6b0d083ac0de4ac51c8
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-15 16:06:32 -0300

    Feat: Entidade para posts no fórum

---
+++ src/forum-posts/entities/forum-post.entity.ts
@@ -0,0 +1,43 @@
+import { Forum } from "src/forums/entities/forum.entity";
+import { User } from "src/users/entities/user.entity";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('forum_posts')
+export class ForumPost {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ManyToOne(() => Forum, (forum) => forum.posts, {
+        nullable: false,
+        onDelete: "CASCADE",
+    })
+    @JoinColumn({name: 'forum_id'})
+    forum: Forum;
+
+    @ManyToOne(() => User, (user) => user.forumPosts, {
+        nullable: false,
+        onDelete: 'SET NULL',
+    })
+    @JoinColumn({name: 'user_id'})
+    user: User;
+
+    @Column({type: 'text'})
+    content: string
+
+    @Column({type:'timestamp with local time zone'})
+    postedAt: string;
+    
+    @ManyToOne(() => ForumPost, (post) => post.replies, {
+    nullable: true,
+    onDelete: 'CASCADE',
+    })
+    @JoinColumn({ name: 'parent_post_id' })
+    parentPost: ForumPost | null;
+
+    @Column({ name: 'parent_post_id', nullable: true })
+    parentPostId: string;
+
+    @OneToMany(() => ForumPost, (post) => post.parentPost)
+    replies: ForumPost[];
+}
