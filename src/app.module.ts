4fedfdf33bba47c7b7307a98b6c0cde15e40d867
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-18 15:23:49 -0300

    Feat: Adicionando configurações para a conexão com o banco de dados Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,9 +21,32 @@ import { SchedulesModule } from './schedules/schedules.module';
 import { ForumsModule } from './forums/forums.module';
 import { ForumPostsModule } from './forum-posts/forum-posts.module';
 import { MessagesModule } from './messages/messages.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, RolesModule, CoursesModule, DepartmentsModule, DisciplinesModule, ClassesModule, EnrollmentsModule, VideoLessonsModule, AttendancesModule, PaymentsModule, GradesModule, NewsModule, ActivitiesModule, MaterialsModule, LessonPlansModule, AvailabilitiesModule, SchedulesModule, ForumsModule, ForumPostsModule, MessagesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        
+        autoLoadEntities: true,
+        
+        synchronize: true,
+      }),
+    }),
+    UsersModule, RolesModule, CoursesModule, DepartmentsModule, DisciplinesModule, ClassesModule, EnrollmentsModule, VideoLessonsModule, AttendancesModule, PaymentsModule, GradesModule, NewsModule, ActivitiesModule, MaterialsModule, LessonPlansModule, AvailabilitiesModule, SchedulesModule, ForumsModule, ForumPostsModule, MessagesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
