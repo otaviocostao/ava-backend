aae2aae50bf35ef3bf6129da8e0b1774cab54f4f
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-18 15:12:25 -0300

    Fix: Corrigindo as colunas da entidade Payment e adicionando o relacionamento ManyToOne com User

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -1,23 +1,33 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { PaymentStatus } from '../../common/enums/payment-status.enum';
+import { User } from 'src/users/entities/user.entity';
 
 @Entity('payments')
 export class Payment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
-  student_id: string;
+  @ManyToOne(() => User, (user) => user.payments, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'student_id' })
+  student: User;
 
   @Column({ type: 'decimal', precision: 10, scale: 2, nullable: false })
   amount: number;
 
-  @Column({ type: 'date' })
-  due_date: Date;
+  @Column({ type: 'date', nullable: false })
+  dueDate: Date;
 
-  @Column({ type: 'timestamp', nullable: true })
-  paid_at: Date;
+  @Column({ name: 'paid_at', type: 'timestamp with time zone', nullable: true })
+  paidAt: Date | null;
 
-  @Column({ type: 'enum', enum: PaymentStatus, default: PaymentStatus.PENDING })
+  @Column({
+    type: 'enum',
+    enum: PaymentStatus,
+    default: PaymentStatus.PENDING,
+  })
   status: PaymentStatus;
+
 }
