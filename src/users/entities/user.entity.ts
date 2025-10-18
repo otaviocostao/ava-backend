ddeb276aceb2beefe519cb26961c76a610ec4433
otaviocostao <dev.otaviocostao@gmail.com>
2025-10-18 15:03:27 -0300

    Feat: Adicionando o relacionamento de OneToMany de User com as outras classes

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,6 +3,13 @@ import * as bcrypt from 'bcrypt';
 import { Role } from "src/roles/entities/role.entity";
 import { ForumPost } from "src/forum-posts/entities/forum-post.entity";
 import { Message } from "src/messages/entities/message.entity";
+import { Enrollment } from "src/enrollments/entities/enrollment.entity";
+import { Payment } from "src/payments/entities/payment.entity";
+import { Class } from "src/classes/entities/class.entity";
+import { Availability } from "src/availabilities/entities/availability.entity";
+import { Material } from "src/materials/entities/material.entity";
+import { VideoLesson } from "src/video-lessons/entities/video-lesson.entity";
+import { News } from "src/news/entities/news.entity";
 
 @Entity("users")
 export class User {
@@ -19,7 +26,7 @@ export class User {
     @Column({ name: 'password_hash', select: false })
     password: string;
 
-    @CreateDateColumn({name: 'create_at'})
+    @CreateDateColumn({name: 'create_at', type: 'timestamp with time zone'})
     createdAt: Date;
     
     @UpdateDateColumn({ name: 'updated_at', type: 'timestamp with time zone' })
@@ -39,7 +46,28 @@ export class User {
         inverseJoinColumn: { name: 'role_id', referencedColumnName: 'id' },
     })
     roles: Role[];
+
+    @OneToMany(() => Enrollment, (enrollment) => enrollment.student)
+    enrollments: Enrollment[];
+
+    @OneToMany(() => Payment, (payment) => payment.student)
+    payments: Payment[];
+
+    @OneToMany(() => Class, (classInstance) => classInstance.teacher)
+    classesTaught: Class[];
+
+    @OneToMany(() => Availability, (availability) => availability.teacher)
+    availabilities: Availability[];
     
+    @OneToMany(() => Material, (material) => material.uploadedBy)
+    materialsUploaded: Material[];
+
+    @OneToMany(() => VideoLesson, (videoLesson) => videoLesson.uploadedBy)
+    videoLessonsUploaded: VideoLesson[];
+
+    @OneToMany(() => News, (news) => news.publishedBy)
+    newsPublished: News[];
+
     @OneToMany(() => ForumPost, (post) => post.user)
     forumPosts: ForumPost[];
 
