6a1a04c9f1dd65fef994a55315471b5a0e711143
Otavio <dev.otaviocostao@gmail.com>
2025-10-27 17:13:29 -0300

    Feat: habilitando o Cors para desenvolvimento

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useWebSocketAdapter(new IoAdapter(app));
   
   app.useGlobalPipes(
     new ValidationPipe({
