42a52cda547c92e14e53ac7b7f8cc84c3ef4ad29
Otavio <dev.otaviocostao@gmail.com>
2025-11-06 22:23:51 -0300

    Fix: Corrigindo erros nos testes da app.controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppContextService } from './app-context.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
+    const mockAppContextService = {
+      getApp: jest.fn(),
+    };
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: AppContextService,
+          useValue: mockAppContextService,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
