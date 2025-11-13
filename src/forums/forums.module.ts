67ad5cbb3a036fbbc24574ce5081a9287c1d60bd
RafaelFS01 <rafafigs@outlook.com>
2025-11-13 14:48:37 -0300

    Feat: Adiciona suporte para associar usuários como criadores de fóruns, incluindo a atualização do DTO, serviço e entidade para gerenciar a criação e listagem de fóruns com informações do criador.

--- src/forums/forums.module.ts
+++ src/forums/forums.module.ts
@@ -4,10 +4,11 @@ import { ForumsController } from './forums.controller';
 import { Forum } from './entities/forum.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Class } from 'src/classes/entities/class.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
   imports: [
-      TypeOrmModule.forFeature([Forum, Class]),
+      TypeOrmModule.forFeature([Forum, Class, User]),
     ],
   controllers: [ForumsController],
   providers: [ForumsService],
